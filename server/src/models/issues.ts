bcc92d0160dbc1fab4380384dc21ca26ff2875f1
zachloh <leongaun17@gmail.com>
2022-10-20 22:42:45 +1100

    Create schema for issues on Mongoose

---
+++ server/src/models/issues.ts
@@ -0,0 +1,73 @@
+import { Schema, model, Types } from 'mongoose';
+
+interface IIssue {
+  project: Types.ObjectId;
+  type: string;
+  priority: string;
+  status: string;
+  title: string;
+  description?: string;
+  reporter: Types.ObjectId;
+  assignee?: Types.ObjectId;
+  dueDate: Date;
+  completedAt?: Date;
+}
+
+const issueSchema = new Schema<IIssue>(
+  {
+    project: {
+      type: Schema.Types.ObjectId,
+      ref: 'Project',
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+    },
+    priority: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+    },
+    reporter: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    assignee: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    dueDate: {
+      type: Date,
+      required: true,
+    },
+    completedAt: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Issue = model<IIssue>('Issue', issueSchema);
+
+export default Issue;
