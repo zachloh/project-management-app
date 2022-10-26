20820f1ba8c7a1ee1aa03dcd7ebd8321b371214d
zachloh <leongaun17@gmail.com>
2022-10-26 23:41:08 +1100

    Create bar chart for issues

--- client/src/features/dashboard/components/Dashboard.tsx
+++ client/src/features/dashboard/components/Dashboard.tsx
@@ -1,6 +1,125 @@
 /* eslint-disable import/prefer-default-export */
+import { Card } from '@mantine/core';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  ChartEvent,
+} from 'chart.js';
+import dayjs from 'dayjs';
 import React from 'react';
+import { Bar } from 'react-chartjs-2';
+
+import styles from './Dashboard.module.css';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const generateDates = () => {
+  const today = dayjs();
+  const dates = [];
+
+  for (let i = 0; i < 7; i++) {
+    dates.push(today.subtract(i, 'day').format('DD/MM'));
+  }
+
+  return dates.reverse();
+};
+
+const data = {
+  labels: generateDates(),
+  datasets: [
+    {
+      label: 'Created issues',
+      data: [4, 2, 3, 2, 5, 6, 4],
+      backgroundColor: '#845ef7',
+    },
+    {
+      label: 'Completed issues',
+      data: [2, 1, 2, 1, 3, 4, 3],
+      backgroundColor: '#a9abe4',
+    },
+  ],
+};
+
+const options = {
+  maintainAspectRatio: false,
+  plugins: {
+    legend: {
+      position: 'top' as const,
+      labels: {
+        boxWidth: 14,
+        boxHeight: 14,
+        font: {
+          weight: '700',
+          size: 14,
+        },
+      },
+      onHover: (event: ChartEvent) => {
+        const element = event.native?.target as HTMLElement;
+        element.style.cursor = 'pointer';
+      },
+      onLeave: (event: ChartEvent) => {
+        const element = event.native?.target as HTMLElement;
+        element.style.cursor = 'default';
+      },
+    },
+    title: {
+      display: true,
+      text: 'Last 7 Days',
+      font: {
+        size: 18,
+        weight: '900',
+      },
+    },
+  },
+  scales: {
+    x: {
+      grid: {
+        borderDash: [3],
+      },
+      ticks: {
+        font: {
+          weight: '700',
+          size: 13,
+        },
+      },
+    },
+    y: {
+      grid: {
+        borderDash: [3],
+      },
+      ticks: {
+        font: {
+          weight: '700',
+          size: 14,
+        },
+      },
+    },
+  },
+};
 
 export function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <div className={styles.container}>
+      <div>
+        <h2 className={styles['card-title']}>Statistics</h2>
+        <Card shadow="sm" p="sm" radius="sm" withBorder>
+          <div className={styles.chart}>
+            <Bar data={data} options={options} />
+          </div>
+        </Card>
+      </div>
+    </div>
+  );
 }
