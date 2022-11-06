243bf31a1885199e7275d7b300cfdd04b2e75e80
zachloh <leongaun17@gmail.com>
2022-11-06 23:39:12 +1100

    Add icons for issue priority

---
+++ client/src/assets/icons/HighPriorityIcon.tsx
@@ -0,0 +1,7 @@
+/* eslint-disable import/prefer-default-export */
+import React from 'react';
+import { ChevronUp } from 'tabler-icons-react';
+
+export function HighPriorityIcon() {
+  return <ChevronUp color="#e31c3d" size={20} />;
+}
---
+++ client/src/assets/icons/MediumPriorityIcon.tsx
@@ -0,0 +1,7 @@
+/* eslint-disable import/prefer-default-export */
+import React from 'react';
+import { Equal } from 'tabler-icons-react';
+
+export function MediumPriorityIcon() {
+  return <Equal color="orange" size={16} />;
+}
