5e9120e71a95061be9ebe9ea96c929bdee4c7903
zachloh <leongaun17@gmail.com>
2022-11-08 18:12:21 +1100

    Create schema for organizations on Mongoose

---
+++ server/src/models/organizations.ts
@@ -0,0 +1,45 @@
+import { Schema, model, Types } from 'mongoose';
+
+interface IOrganization {
+  name: string;
+  members: Types.ObjectId[];
+  projects: Types.ObjectId[];
+}
+
+const organizationSchema = new Schema<IOrganization>(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    members: {
+      type: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'User',
+          required: true,
+        },
+      ],
+      validate: {
+        validator(array: Types.ObjectId[]) {
+          return array.length > 0;
+        },
+        message: () => 'members should not be empty',
+      },
+    },
+    projects: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Project',
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Organization = model<IOrganization>('Organization', organizationSchema);
+
+export default Organization;
