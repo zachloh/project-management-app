f8168381eab836f5473e35704cac7d8556c36244
zachloh <leongaun17@gmail.com>
2022-11-10 18:28:49 +1100

    Add more routes on the client app

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 import Layout from 'components/Layout';
+import { KanbanBoard } from 'features/kanban-board';
 import { OverviewDashboard } from 'features/overview-dashboard';
+import { ProjectDashboard } from 'features/project-dashboard';
 
 function AppRoutes() {
   return (
     <Layout>
       <Routes>
-        <Route path="/" element={<OverviewDashboard />} />
+        <Route path="/" element={<Navigate to="/dashboard" replace />} />
+        <Route path="/dashboard" element={<OverviewDashboard />} />
+        <Route path="/dashboard/:projectId" element={<ProjectDashboard />} />
+        <Route path="/projects/:projectId" element={<KanbanBoard />} />
       </Routes>
     </Layout>
   );
