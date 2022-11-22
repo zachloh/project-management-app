2ced8628fa5b834b4a143390cf2f030e166f24e1
zachloh <leongaun17@gmail.com>
2022-11-22 21:34:12 +1100

    Increase the size of MediumPriorityIcon

--- client/src/assets/icons/MediumPriorityIcon.tsx
+++ client/src/assets/icons/MediumPriorityIcon.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import { Equal } from 'tabler-icons-react';
 
 export function MediumPriorityIcon() {
-  return <Equal color="orange" size={16} />;
+  return <Equal color="orange" size={20} />;
 }
