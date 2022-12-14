cba0d78579685216aaaab563bfb11faaad08fc3d
zachloh <leongaun17@gmail.com>
2022-12-14 17:02:59 +1100

    Add validation for RegisterForm

--- client/src/features/auth/components/RegisterForm.tsx
+++ client/src/features/auth/components/RegisterForm.tsx
@@ -1,28 +1,96 @@
 import {
   Anchor,
+  Box,
   Button,
   Divider,
   PasswordInput,
   Text,
   TextInput,
 } from '@mantine/core';
+import { useForm, zodResolver } from '@mantine/form';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Check } from 'tabler-icons-react';
+import { z } from 'zod';
 
+import { RegisterFormValues } from '../types';
 import AuthLayout from './AuthLayout';
 
+const registerSchema = z.object({
+  firstName: z
+    .string()
+    .trim()
+    .min(1, { message: 'Please enter your first name' }),
+  lastName: z
+    .string()
+    .trim()
+    .min(1, { message: 'Please enter your last name' }),
+  email: z
+    .string()
+    .email({ message: 'Invalid email address' })
+    .min(1, { message: 'Please enter your email address' }),
+  password: z
+    .string()
+    .min(6, { message: 'Password must contain at least 6 characters' }),
+});
+
 export function RegisterForm() {
+  const form = useForm<RegisterFormValues>({
+    initialValues: {
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+    },
+    validate: zodResolver(registerSchema),
+    validateInputOnChange: ['password'],
+  });
+
+  const handleSubmit = (values: RegisterFormValues) => {
+    console.log(values);
+  };
+
   return (
     <AuthLayout title="Sign Up">
-      <form>
-        <TextInput label="First Name" mb={20} />
-        <TextInput label="Last Name" mb={20} />
-        <TextInput label="Email" mb={20} />
+      <form onSubmit={form.onSubmit(handleSubmit)}>
+        <TextInput
+          label="First Name"
+          mb={20}
+          {...form.getInputProps('firstName')}
+        />
+        <TextInput
+          label="Last Name"
+          mb={20}
+          {...form.getInputProps('lastName')}
+        />
+        <TextInput label="Email" mb={20} {...form.getInputProps('email')} />
         <PasswordInput
           label="Password"
-          description="Password must contain at least 6 characters"
+          description={
+            form.errors.password || form.isValid('password')
+              ? null
+              : 'Password must contain at least 6 characters'
+          }
+          inputWrapperOrder={['label', 'input', 'description', 'error']}
+          styles={{
+            error: {
+              marginTop: 5,
+            },
+          }}
+          {...form.getInputProps('password')}
         />
-        <Button w="100%" mt={30} size="md" color="violet.5">
+        {form.isValid('password') && (
+          <Text
+            size={12}
+            color="teal.9"
+            mt={5}
+            sx={{ display: 'flex', alignItems: 'center' }}
+          >
+            <Check size={14} />
+            <Box ml={5}>Password must contain at least 6 characters</Box>
+          </Text>
+        )}
+        <Button type="submit" w="100%" mt={30} size="md" color="violet.5">
           Sign Up
         </Button>
       </form>
--- client/src/features/auth/types/index.ts
+++ client/src/features/auth/types/index.ts
@@ -2,3 +2,8 @@ export interface LoginFormValues {
   email: string;
   password: string;
 }
+
+export interface RegisterFormValues extends LoginFormValues {
+  firstName: string;
+  lastName: string;
+}
