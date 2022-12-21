c0eaef2b3f1ec8868caa279e66401421ea82b757
zachloh <leongaun17@gmail.com>
2022-12-21 18:27:44 +1100

    Add mutation for updating a user's organization

---
+++ client/src/api/users/updateUserOrg.ts
@@ -0,0 +1,39 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+import { customAxios } from 'lib/axios';
+import { User } from 'types';
+
+type UserData = {
+  org: string;
+  position: string;
+  role: 'admin' | 'project manager' | 'member';
+};
+
+const updateUserOrg = async ({
+  userId,
+  userData,
+}: {
+  userId: string | undefined;
+  userData: UserData;
+}): Promise<User> => {
+  if (typeof userId === 'undefined') {
+    return Promise.reject(new Error('Invalid userId'));
+  }
+
+  const { data } = await customAxios.patch<User>(
+    `/users/${userId}/org`,
+    userData
+  );
+  return data;
+};
+
+export const useUpdateUserOrg = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: updateUserOrg,
+    onSuccess: (data) => {
+      queryClient.setQueryData(['auth-user'], data);
+    },
+  });
+};
