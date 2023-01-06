e24d492ada22f795983e57c261eb2ca1480055b4
zachloh <leongaun17@gmail.com>
2023-01-06 19:16:49 +1100

    Create projectHistory schema on mongoose

---
+++ server/src/models/projectHistory.ts
@@ -0,0 +1,79 @@
+import { model, Schema, Types } from 'mongoose';
+
+export interface History {
+  issueId: Types.ObjectId;
+  issueTitle: string;
+  user: Types.ObjectId;
+  mutation: 'create' | 'update' | 'delete';
+  updatedFields?: string[];
+  date: Date;
+  isDeleted: boolean;
+}
+
+interface IProjectHistory {
+  projectId: Types.ObjectId;
+  history: History[];
+}
+
+const projectHistorySchema = new Schema<IProjectHistory>({
+  projectId: {
+    type: Schema.Types.ObjectId,
+    required: true,
+  },
+  history: [
+    {
+      issueId: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'Issue',
+      },
+      issueTitle: {
+        type: String,
+        required: true,
+      },
+      user: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'User',
+      },
+      mutation: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        enum: ['create', 'update', 'delete'],
+      },
+      updatedFields: [
+        {
+          type: String,
+          trim: true,
+          enum: [
+            'type',
+            'priority',
+            'status',
+            'title',
+            'description',
+            'reporter',
+            'assignee',
+            'dueDate',
+          ],
+        },
+      ],
+      date: {
+        type: Date,
+        required: true,
+      },
+      isDeleted: {
+        type: Boolean,
+        default: false,
+      },
+    },
+  ],
+});
+
+const ProjectHistory = model<IProjectHistory>(
+  'ProjectHistory',
+  projectHistorySchema
+);
+
+export default ProjectHistory;
