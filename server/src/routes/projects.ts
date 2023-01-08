37d5c8f60566f2505bb23794de64fe71288a1edc
zachloh <leongaun17@gmail.com>
2023-01-08 20:10:17 +1100

    Add API route for getting a project history

--- server/src/routes/projects.ts
+++ server/src/routes/projects.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 
+import projectHistoryController from 'controllers/projectHistory';
 import projectsController from 'controllers/projects';
 
 const router = express.Router();
@@ -12,5 +13,8 @@ router.post('/', projectsController.createProject);
 
 // GET /api/projects/:projectId
 router.get('/:projectId', projectsController.getProjectById);
+
+// GET /api/projects/:projectId/history
+router.get('/:projectId/history', projectHistoryController.getProjectHistory);
 
 export default router;
