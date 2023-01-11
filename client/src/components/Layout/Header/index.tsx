006014f233c35fce8efd085146007e2ad6f5a090
zachloh <leongaun17@gmail.com>
2023-01-11 18:29:30 +1100

    Convert logo to a Link to homepage

--- client/src/components/Layout/Header/index.tsx
+++ client/src/components/Layout/Header/index.tsx
@@ -6,8 +6,10 @@ import {
   Grid,
   Center,
   Title,
+  Anchor,
 } from '@mantine/core';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Box } from 'tabler-icons-react';
 
 import styles from './Header.module.css';
@@ -21,12 +23,14 @@ function Header({ opened, onToggleNavbar }: HeaderProps) {
   return (
     <Grid align="center" h="100%" m={0}>
       <Grid.Col span="auto" p={0}>
-        <Group spacing={4}>
-          <Box size={48} color="#845EF7" />
-          <MediaQuery smallerThan="sm" styles={{ display: 'none' }}>
-            <Text className={styles.logo}>ProjectHub</Text>
-          </MediaQuery>
-        </Group>
+        <Anchor component={Link} to="/dashboard" underline={false}>
+          <Group spacing={4}>
+            <Box size={48} color="#845EF7" />
+            <MediaQuery smallerThan="sm" styles={{ display: 'none' }}>
+              <Text className={styles.logo}>ProjectHub</Text>
+            </MediaQuery>
+          </Group>
+        </Anchor>
       </Grid.Col>
       <Grid.Col span={4} p={0}>
         <Center>
