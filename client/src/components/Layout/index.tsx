11680df92cd6cdbed5f14fec1beaff6c048ee8b2
zachloh <leongaun17@gmail.com>
2023-01-12 23:20:28 +1100

    Refactor Layout to use Mantine AppShell

--- client/src/components/Layout/index.tsx
+++ client/src/components/Layout/index.tsx
@@ -1,17 +1,37 @@
-import React from 'react';
+import { AppShell, Header, Navbar } from '@mantine/core';
+import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 
-import Header from './Header';
-import styles from './Layout.module.css';
+import HeaderContent from './Header';
+import NavbarContent from './Navbar';
 
 function Layout() {
+  const [opened, setOpened] = useState(false);
+
   return (
-    <>
-      <Header />
-      <main className={styles.main}>
-        <Outlet />
-      </main>
-    </>
+    <AppShell
+      padding={16}
+      header={
+        <Header height={70} px="md">
+          <HeaderContent
+            opened={opened}
+            onToggleNavbar={() => setOpened((o) => !o)}
+          />
+        </Header>
+      }
+      navbar={
+        <Navbar
+          p="md"
+          hiddenBreakpoint="sm"
+          hidden={!opened}
+          width={{ sm: 250, lg: 300 }}
+        >
+          <NavbarContent />
+        </Navbar>
+      }
+    >
+      <Outlet />
+    </AppShell>
   );
 }
 
