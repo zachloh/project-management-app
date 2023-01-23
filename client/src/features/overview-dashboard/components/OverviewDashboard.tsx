0d0ed6e1ce201988a3d25499c35a71e56d6ac27e
zachloh <leongaun17@gmail.com>
2023-01-23 18:05:24 +1100

    Replace project cards Grid with SimpleGrid

--- client/src/features/overview-dashboard/components/OverviewDashboard.tsx
+++ client/src/features/overview-dashboard/components/OverviewDashboard.tsx
@@ -1,4 +1,4 @@
-import { Grid } from '@mantine/core';
+import { SimpleGrid } from '@mantine/core';
 import React from 'react';
 
 import { useGetProjects } from 'api/projects/getProjects';
@@ -6,6 +6,8 @@ import { useGetUser } from 'api/users/getUser';
 import Card from 'components/Card/Card';
 import CardHeader from 'components/Card/CardHeader';
 import DashboardSkeleton from 'components/DashboardSkeleton';
+import DashboardTabs from 'components/DashboardTabs';
+import MainHeading from 'components/MainHeading';
 import ProjectCard from 'components/ProjectCard';
 
 import IssuesChart from './IssuesChart';
@@ -34,38 +36,51 @@ export function OverviewDashboard() {
   // TODO: Handle error (add 'unexpected error' component)
 
   return (
-    <div className={styles.container}>
-      <div>
-        <CardHeader>Statistics</CardHeader>
-        <div className={styles.statistics}>
+    <>
+      <MainHeading />
+      <DashboardTabs />
+      <div className={styles.container}>
+        <div>
+          <CardHeader>Statistics</CardHeader>
+          <div className={styles.statistics}>
+            <Card>
+              <IssuesChart
+                createdIssues={data.createdIssuesLast7Days}
+                completedIssues={data.completedIssuesLast7Days}
+              />
+            </Card>
+            <Card title="Overall Progress">
+              <IssuesProgress projects={data.projects} />
+            </Card>
+          </div>
+        </div>
+        <div>
+          <CardHeader>Your Projects</CardHeader>
+          <SimpleGrid
+            spacing="lg"
+            breakpoints={[
+              { minWidth: 'xs', cols: 2 },
+              { minWidth: 'sm', cols: 1 },
+              { minWidth: 850, cols: 2 },
+              { minWidth: 'lg', cols: 3 },
+              { minWidth: 'xl', cols: 4 },
+              { minWidth: 2000, cols: 5 },
+              { minWidth: 2560, cols: 6 },
+            ]}
+          >
+            {/* TODO: check if projects array is empty */}
+            {data.projects.map((project) => (
+              <ProjectCard project={project} />
+            ))}
+          </SimpleGrid>
+        </div>
+        <div>
+          <CardHeader>Organization Members</CardHeader>
           <Card>
-            <IssuesChart
-              createdIssues={data.createdIssuesLast7Days}
-              completedIssues={data.completedIssuesLast7Days}
-            />
-          </Card>
-          <Card title="Overall Progress">
-            <IssuesProgress projects={data.projects} />
+            <MembersTable orgId={user?.org?._id} />
           </Card>
         </div>
       </div>
-      <div>
-        <CardHeader>Your Projects</CardHeader>
-        <Grid gutter="sm">
-          {/* TODO: check if projects array is empty */}
-          {data.projects.map((project) => (
-            <Grid.Col key={project._id} xs={12} sm={6} md={4} lg={3}>
-              <ProjectCard project={project} />
-            </Grid.Col>
-          ))}
-        </Grid>
-      </div>
-      <div>
-        <CardHeader>Organization Members</CardHeader>
-        <Card>
-          <MembersTable />
-        </Card>
-      </div>
-    </div>
+    </>
   );
 }
