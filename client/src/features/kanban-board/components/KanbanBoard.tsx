2f1879295d83f28e30bb81ca66efcf8a9ea4bccb
zachloh <leongaun17@gmail.com>
2023-01-30 18:16:44 +1100

    Add filters for issues

--- client/src/features/kanban-board/components/KanbanBoard.tsx
+++ client/src/features/kanban-board/components/KanbanBoard.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import { useDebouncedValue } from '@mantine/hooks';
+import React, { useState } from 'react';
 import { DragDropContext, DropResult } from 'react-beautiful-dnd';
 import { useParams } from 'react-router-dom';
 
@@ -7,18 +8,60 @@ import {
   useUpdateIssueStatus,
 } from 'api/issues/updateIssueStatus';
 import { useGetProject } from 'api/projects/getProject';
+import { useGetUser } from 'api/users/getUser';
 import MainHeading from 'components/MainHeading';
+import { Issue } from 'types';
 
 import IssueModal from './IssueModal';
+import IssuesFilter from './IssuesFilter';
 import styles from './KanbanBoard.module.css';
 import KanbanCard from './KanbanCard';
 
 export function KanbanBoard() {
   const { projectId } = useParams();
 
-  const { data: project, isLoading, isError } = useGetProject(projectId);
+  const {
+    data: user,
+    isLoading: isLoadingUser,
+    isError: isUserError,
+  } = useGetUser();
+
+  const {
+    data: project,
+    isLoading: isLoadingProject,
+    isError: isProjectError,
+  } = useGetProject(projectId);
 
   const updateIssueStatusMutation = useUpdateIssueStatus(project?._id || '');
+
+  const [titleFilter, setTitleFilter] = useState('');
+  const [debouncedTitleFilter] = useDebouncedValue(titleFilter, 350);
+  const [filteredAssignees, setFilteredAssignees] = useState<string[]>([]);
+
+  const filterIssues = (issues: Issue[]) => {
+    if (titleFilter.length === 0 && filteredAssignees.length === 0) {
+      return issues;
+    }
+
+    let filteredIssues = [...issues];
+
+    if (filteredAssignees.length > 0) {
+      filteredIssues = filteredIssues.filter((issue) => {
+        if (!issue.assignee) {
+          return false;
+        }
+        return filteredAssignees.includes(issue.assignee);
+      });
+    }
+
+    if (debouncedTitleFilter.length > 0) {
+      filteredIssues = filteredIssues.filter((issue) =>
+        issue.title.toLowerCase().includes(debouncedTitleFilter.toLowerCase())
+      );
+    }
+
+    return filteredIssues;
+  };
 
   const onDragEnd = (result: DropResult) => {
     const { draggableId: issueId, source, destination } = result;
@@ -44,44 +87,59 @@ export function KanbanBoard() {
   };
 
   // TODO: Add skeleton
-  if (isLoading) {
+  if (isLoadingUser || isLoadingProject) {
     return <div>Loading...</div>;
   }
 
   // TODO: Add error component
-  if (isError) {
+  if (isUserError || isProjectError || !user) {
     return <div>Error...</div>;
   }
 
   return (
     <>
       <MainHeading title={project.name} />
+      <IssuesFilter
+        titleFilter={titleFilter}
+        setTitleFilter={setTitleFilter}
+        filteredAssignees={filteredAssignees}
+        setFilteredAssignees={setFilteredAssignees}
+        members={project.members}
+      />
       <DragDropContext onDragEnd={onDragEnd}>
         <div className={styles.container}>
-          <IssueModal />
+          <IssueModal members={project.members} />
           <KanbanCard
             title="TO DO"
-            issues={project.todoIssues}
+            issues={filterIssues(project.todoIssues)}
             id="todoIssues"
             projectId={project._id}
+            userId={user._id}
+            members={project.members}
           />
           <KanbanCard
             title="IN PROGRESS"
-            issues={project.inProgressIssues}
+            issues={filterIssues(project.inProgressIssues)}
             id="inProgressIssues"
             projectId={project._id}
+            userId={user._id}
+            members={project.members}
           />
           <KanbanCard
             title="IN REVIEW"
-            issues={project.inReviewIssues}
+            issues={filterIssues(project.inReviewIssues)}
             id="inReviewIssues"
             projectId={project._id}
+            userId={user._id}
+            members={project.members}
           />
           <KanbanCard
             title="DONE"
-            issues={project.completedIssues}
+            issues={filterIssues(project.completedIssues)}
             id="completedIssues"
             projectId={project._id}
+            userId={user._id}
+            members={project.members}
           />
         </div>
       </DragDropContext>
