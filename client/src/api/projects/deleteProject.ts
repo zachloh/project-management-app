c8b78a8ea8c52801c0d47a723d446f11a6b394c9
zachloh <leongaun17@gmail.com>
2023-02-02 16:43:18 +1100

    Add mutation for deleting a project

---
+++ client/src/api/projects/deleteProject.ts
@@ -0,0 +1,56 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+import { customAxios } from 'lib/axios';
+import { Project } from 'types';
+
+import { GetProjectsResponse } from './getProjects';
+
+const deleteProject = async ({
+  projectId,
+  orgId,
+}: {
+  projectId: string;
+  orgId: string | undefined;
+}): Promise<Project<string, string>> => {
+  if (typeof orgId === 'undefined') {
+    return Promise.reject(new Error('Invalid orgId'));
+  }
+
+  const { data } = await customAxios.delete<Project<string, string>>(
+    `/projects/${projectId}?orgId=${orgId}`
+  );
+  return data;
+};
+
+export const useDeleteProject = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: deleteProject,
+    onSuccess: (_, variables) => {
+      if (variables.orgId) {
+        queryClient.setQueryData<GetProjectsResponse>(
+          ['org', variables.orgId, 'projects'],
+          (old) => {
+            if (old) {
+              const newProjects = old.projects.filter(
+                (project) => project._id !== variables.projectId
+              );
+
+              return {
+                ...old,
+                projects: newProjects,
+              };
+            }
+            return old;
+          }
+        );
+      }
+
+      queryClient.removeQueries({
+        queryKey: ['projects', variables.projectId],
+        exact: true,
+      });
+    },
+  });
+};
