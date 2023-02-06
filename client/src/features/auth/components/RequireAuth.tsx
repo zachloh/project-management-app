4bd8622ca7eb6ae6c054e925fb60e98e798f1025
zachloh <leongaun17@gmail.com>
2023-02-06 16:26:49 +1100

    Refetch user on route change

--- client/src/features/auth/components/RequireAuth.tsx
+++ client/src/features/auth/components/RequireAuth.tsx
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 
 import { useAuth } from 'api/auth';
 import { useGetUser } from 'api/users/getUser';
+import Layout from 'components/Layout';
 import Loader from 'components/Loader';
 
-type RequireAuthProps = {
-  children: React.ReactNode;
-};
-
-export function RequireAuth({ children }: RequireAuthProps) {
-  const { data: user, isLoading, isError } = useGetUser();
+export function RequireAuth() {
+  const { data: user, isLoading, isError, refetch } = useGetUser();
   const { logout } = useAuth();
   const location = useLocation();
+
+  useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    refetch({ cancelRefetch: false });
+  }, [location.pathname, refetch]);
 
   if (isLoading) {
     return <Loader />;
@@ -25,7 +27,7 @@ export function RequireAuth({ children }: RequireAuthProps) {
 
   if (user) {
     if (user.completedWelcome) {
-      return <>{children}</>;
+      return <Layout user={user} />;
     }
     return <Navigate to="/welcome" replace />;
   }
