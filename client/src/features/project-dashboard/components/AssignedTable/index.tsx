81282325795379bc82ab163250f376342e84492a
zachloh <leongaun17@gmail.com>
2023-02-17 16:53:58 +1100

    Show users if no issues assigned

--- client/src/features/project-dashboard/components/AssignedTable/index.tsx
+++ client/src/features/project-dashboard/components/AssignedTable/index.tsx
@@ -1,4 +1,4 @@
-import { Table, ScrollArea, MediaQuery } from '@mantine/core';
+import { Table, ScrollArea, MediaQuery, Text } from '@mantine/core';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
@@ -11,21 +11,27 @@ type AssignedTableProps = {
   project: Project;
 };
 
+const filterAssignedIssues = (project: Project, userId: string) => {
+  const { todoIssues, inProgressIssues, inReviewIssues, completedIssues } =
+    project;
+
+  return todoIssues
+    .concat(inProgressIssues, inReviewIssues, completedIssues)
+    .filter((issue) => issue.assignee === userId);
+};
+
 const mapAssignedIssues = (
   issues: Issue[],
-  userId: string,
   onClick: (issueId: string) => void
 ) =>
-  issues
-    .filter((issue) => issue.assignee === userId)
-    .map((issue) => (
-      <tr key={issue._id} onClick={() => onClick(issue._id)}>
-        <MediaQuery smallerThan={360} styles={{ display: 'none' }}>
-          <td className={styles.status}>{issue.status}</td>
-        </MediaQuery>
-        <td className={styles.title}>{issue.title}</td>
-      </tr>
-    ));
+  issues.map((issue) => (
+    <tr key={issue._id} onClick={() => onClick(issue._id)}>
+      <MediaQuery smallerThan={360} styles={{ display: 'none' }}>
+        <td className={styles.status}>{issue.status}</td>
+      </MediaQuery>
+      <td className={styles.title}>{issue.title}</td>
+    </tr>
+  ));
 
 function AssignedTable({ project }: AssignedTableProps) {
   const { user } = useUser();
@@ -35,7 +41,7 @@ function AssignedTable({ project }: AssignedTableProps) {
     navigate(`/projects/${project._id}?selectedIssue=${issueId}`);
   };
 
-  // TODO: Check if no issue assigned
+  const filteredIssues = filterAssignedIssues(project, user._id);
 
   return (
     <ScrollArea h={300} px={12} pb={12} mt={12}>
@@ -49,16 +55,15 @@ function AssignedTable({ project }: AssignedTableProps) {
           </tr>
         </thead>
         <tbody>
-          {mapAssignedIssues(project.todoIssues, user._id, handleRowClick)}
-          {mapAssignedIssues(
-            project.inProgressIssues,
-            user._id,
-            handleRowClick
-          )}
-          {mapAssignedIssues(project.inReviewIssues, user._id, handleRowClick)}
-          {mapAssignedIssues(project.completedIssues, user._id, handleRowClick)}
+          {filteredIssues.length > 0 &&
+            mapAssignedIssues(filteredIssues, handleRowClick)}
         </tbody>
       </Table>
+      {filteredIssues.length === 0 && (
+        <Text align="center" size={14} weight={600} color="dark.4" pt={10}>
+          No issues assigned to you.
+        </Text>
+      )}
     </ScrollArea>
   );
 }
