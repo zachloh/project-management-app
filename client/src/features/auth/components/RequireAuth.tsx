6411269b9114b71e8ae3d079ec0cfbf0befb7f04
zachloh <leongaun17@gmail.com>
2023-02-19 22:08:59 +1100

    Handle unexpected/server error

--- client/src/features/auth/components/RequireAuth.tsx
+++ client/src/features/auth/components/RequireAuth.tsx
@@ -1,3 +1,5 @@
+import { useQueryClient } from '@tanstack/react-query';
+import axios from 'axios';
 import React, { useEffect } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 
@@ -5,11 +7,13 @@ import { useAuth } from 'api/auth';
 import { useGetUser } from 'api/users/getUser';
 import Layout from 'components/Layout';
 import Loader from 'components/Loader';
+import ServerError from 'components/ServerError';
 
 export function RequireAuth() {
-  const { data: user, isLoading, isError, refetch } = useGetUser();
+  const { data: user, isLoading, isError, error, refetch } = useGetUser();
   const { logout } = useAuth();
   const location = useLocation();
+  const queryClient = useQueryClient();
 
   useEffect(() => {
     // eslint-disable-next-line @typescript-eslint/no-floating-promises
@@ -21,8 +25,17 @@ export function RequireAuth() {
   }
 
   if (isError) {
-    logout();
-    return <Navigate to="/login" replace state={{ path: location.pathname }} />;
+    if (axios.isAxiosError(error) && error.response?.status === 401) {
+      logout();
+      return (
+        <Navigate to="/login" replace state={{ path: location.pathname }} />
+      );
+    }
+
+    queryClient.removeQueries({ queryKey: ['org'] });
+    queryClient.removeQueries({ queryKey: ['projects'] });
+    queryClient.removeQueries({ queryKey: ['issues'] });
+    return <ServerError />;
   }
 
   if (user) {
