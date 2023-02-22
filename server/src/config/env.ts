7444dbbf2225cdf1b0fa71dc2a4c3db1093e83b3
zachloh <leongaun17@gmail.com>
2023-02-23 00:58:47 +1100

    Add guest user password to .env

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -12,6 +12,9 @@ const env = {
     accessSecret: process.env.ACCESS_TOKEN_SECRET as string,
     expireTime: process.env.TOKEN_EXPIRE_TIME as string,
   },
+  demoData: {
+    password: process.env.DEMO_PASSWORD as string,
+  },
 };
 
 export default env;
